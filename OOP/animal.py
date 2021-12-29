c3f52ab6b4442cc7916f7f5b34388da59652380e
ecmagnuson <ecmagnuson@wisc.edu>
2021-12-28 18:10:45 -0600

    Python

---
+++ OOP/animal.py
@@ -0,0 +1,15 @@
+class Animal:
+    
+    def __init__(self, species, age, weight):
+        self.species = species
+        self.age = age
+        self.weight = weight
+    
+    def print_info(self):
+        print(self.species)
+        print(self.age)
+        print(self.weight)
+        
+#seal = Animal('seal', 20, 200)
+
+#seal.print_info()
---
+++ OOP/create_animals.py
@@ -0,0 +1,6 @@
+import animal
+
+seal = animal.Animal('seal', 20, 200)
+
+seal.print_info()
+
